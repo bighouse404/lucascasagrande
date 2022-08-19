32dd61cbbf539c4f9d0250b8a927aadb87be2eea
Lucas <lucas.casagrande0120@gmail.com>
2022-08-19 16:16:13 -0300

    new form added

--- teste/send-mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $nome = $_POST['nome'];
-  $email= $_POST['email'];
-  $mensagem= $_POST['mensagem'];
-  $to = "lucas.casagrande0120@gmail.com";
-  $assunto = "Mensagem de ".$email.com
-  mail($to,$assunto,$mensagem);
-?>
